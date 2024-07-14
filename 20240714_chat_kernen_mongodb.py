cd412d692baa7e81ff3b39ab36e79e24519e40c4
yulialitvinova <litvinova.y@gmail.com>
2024-07-14 23:06:35 +0200

    mongodb

--- 20240714_chat_kernen_mongodb.py
+++ 20240714_chat_kernen_mongodb.py
@@ -50,11 +50,12 @@ import pymongo
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from urllib.parse import quote_plus
-from langchain_mongodb import MongoDBAtlasVectorSearch
+##############from langchain_mongodb import MongoDBAtlasVectorSearch
+from langchain_mongodb_vectorstores import MongoDBAtlasVectorSearch
 
 #from langchain_community.utilities import GoogleSearchAPIWrapper
 ##############from langchain_google_community import GoogleSearchAPIWrapper
-from search import GoogleSearchAPIWrapper
+from langchain_google_community_search import GoogleSearchAPIWrapper
 # from langchain_community.tools import DuckDuckGoSearchRun
 # from langchain_community.utilities import SearchApiAPIWrapper
 # from langchain_community.utilities import SerpAPIWrapper

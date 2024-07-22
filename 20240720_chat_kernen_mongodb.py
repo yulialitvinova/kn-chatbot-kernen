d35878a4e9a45e19f060f99f795ea33220d514f5
yulialitvinova <litvinova.y@gmail.com>
2024-07-22 16:45:47 +0200

    change_prompt

--- 20240720_chat_kernen_mongodb.py
+++ 20240720_chat_kernen_mongodb.py
@@ -423,7 +423,7 @@ tools = [
     Tool(
         name="simple_search_googleapiwrapper",
         func=simple_search.run,
-        description="Tool to search Internet for general information after 2021. Helpful to provide information on today news or upcoming events.",
+        description="Tool to search Internet for general current information, i.e., information after 2021. Helpful to provide information on news or upcoming events.",
     ),
     # Tool(
     #     name="simple_search_serpapi",
